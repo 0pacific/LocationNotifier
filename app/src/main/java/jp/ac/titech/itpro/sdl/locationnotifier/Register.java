4fc28f6c27c440d1b62e95fa410b49ecf1e7e289
Taihei Ogitani <joetsu.saiko@gmail.com>
2016-07-11 16:30:17 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/locationnotifier/Register.java
@@ -0,0 +1,17 @@
+package jp.ac.titech.itpro.sdl.locationnotifier;
+
+import java.util.Date;
+
+/**
+ * Created by admin on 16/07/11.
+ */
+public class Register {
+
+    Date date;
+    String mail;
+
+    public Register (Date date,String mail) {
+        this.date = date;
+        this.mail = mail;
+    }
+}
